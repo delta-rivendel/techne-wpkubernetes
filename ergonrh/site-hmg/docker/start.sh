dd5d5b3edca377d797dfaabc4646fea55c0da974
delta-rivendel <delta@rivendel.com.br>
2019-03-21 15:43:09 -0300

    Final Lucas

--- ergonrh/site-hmg/docker/start.sh
+++ ergonrh/site-hmg/docker/start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-WP_PATH="/usr/share/nginx/intratechne"
+WP_PATH="/usr/share/nginx/ergonrh"
 find $WP_PATH -type f -not -path "*wp-content*" -exec chmod 755 {} \;
 find $WP_PATH -type f -not -path "*wp-content*" -exec chown www-data:www-data {} \;
 find $WP_PATH -type d -not -path "*wp-content*" -exec chmod 755 {} \;
